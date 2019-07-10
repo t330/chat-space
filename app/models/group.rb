7ac7e1c42a9c8c6cbf426d8b2191c9a9ddff9235
Tatsuji Haga <blueandaqualine@gmail.com>
2019-07-10 18:05:38 +0900

    Updated db

---
+++ app/models/group.rb
@@ -0,0 +1,31 @@
+class Group < ApplicationRecord
+  has_many :group_users
+  has_many :users, through: :group_users
+  has_many :messages
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
+  def show_last_group
+    if (last_group = groups.last).present?
+      last_group.content? ? last_group.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
+  def show_last_group
+    if (last_group = groups.last).present?
+      last_group.content? ? last_group.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
+
+end
